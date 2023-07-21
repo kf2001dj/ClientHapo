e52025da3febe6ad2413e8fb3e9c2cc6455d1bb3
ThanhDong <cntt23022019@gmail.com>
2023-07-21 17:51:03 +0700

    add  edit profile and update

--- src/components/Profile/Body_Profile.js
+++ src/components/Profile/Body_Profile.js
@@ -1,27 +1,11 @@
 import React, { useState, useEffect } from 'react';
-
 import './Body_Profile.css'
 
 
 export default function Body_Profile()
 {
     const [user, setUser] = useState({});
-    // const userId = "1"; 
     const userId = localStorage.getItem('userId');
-    // useEffect(() => {
-    //   // Gọi API để lấy dữ liệu từ server Express sử dụng ID người dùng
-    //   fetch(`http://localhost:4000/api/users/${userId}`)
-    //     .then((response) => {
-    //       if (!response.ok) {
-    //         throw new Error('Response was not ok');
-    //       }
-    //       return response.json();
-    //     })
-    //     .then((data) => setUser(data))
-    //     .catch((error) => {
-    //       console.error('Lỗi khi gọi API: ', error);
-    //     });
-    // }, [userId]); // Thêm 'userId' vào danh sách dependencies để useEffect chạy lại khi ID người dùng thay đổi
 
     useEffect(() => {
         // Fetch user data using the userId from localStorage
@@ -37,13 +21,64 @@ export default function Body_Profile()
             .catch((error) => {
               console.error('Error fetching user data: ', error);
             });
+        }else {
+            //bắt buộc phải đăng nhập thì ms edit được
+            setUser({});
         }
       }, [userId]);
+
+    const [isEditing, setIsEditing] = useState(false);
+    
+    // State variables to manage form input values
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthdate, setBirthdate] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [about, setAbout] = useState('');
+
+    const handleUpdate = () => {
+    // Create a new user object with updated data
+    const updatedUser = {
+      ...user,
+      name: name || user.name,
+      email: email || user.email,
+      birthdate: birthdate || user.birthdate, // Sử dụng định dạng ngày tháng mới
+      phone: phone || user.phone,
+      address: address || user.address,
+      about: about || user.about,
+    };
+
+    // Update user data on the server
+    fetch(`http://localhost:4000/api/users/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedUser),
+      credentials: 'include',
+    })
+    .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Show success message or handle the response data as needed
+        setUser(updatedUser);
+        console.log('User data updated successfully:', data);
+        
+      })
+      .catch((error) => {
+        console.error('Error updating user data: ', error);
+      });
+    };
+   
 
     return(
         <div className='container text-center'>
             <div className='fontuser-pro'>
-               
                     <div>
                         <img
                         key={user.id}
@@ -59,7 +94,10 @@ export default function Body_Profile()
                         <div className='pro-btnadone'></div>
                         <div className='pro-day'>
                             <img src='./image/sn.png' className='img-pro-sn'></img>
-                            <p className='txt-daypro'>{user.birthdate}</p>
+                            <p className='txt-daypro'
+                            value={birthdate || user.birthdate  }
+                            onChange={(e) => setBirthdate(e.target.value)}
+                            >{user.birthdate}</p>
                         </div>
                         <div className='btn-pro-sn'></div>
                         <div className='pro-phon'>
@@ -77,9 +115,6 @@ export default function Body_Profile()
                             </p>
                         </div>
                     </div>
-                   
-                
-                
             </div>
 
             <div className='fonttext-pro'>
@@ -120,7 +155,7 @@ export default function Body_Profile()
                         <p className='pro-txtaddcour'>Add course</p>
                     </div>
                 </div>
-
+                
                 <div className='pro-edit-file'>
                     <p className='text-editpro'>Edit profile</p>
                     <div className='btn-pro-one'></div>
@@ -132,54 +167,67 @@ export default function Body_Profile()
                         <p className='txtname-pro'>
                             Name:
                         </p>
-                        <input className='ipt-txtname' placeholder='Your name...'></input>
+                        <input className='ipt-txtname' placeholder='Your name...'
+                        value={name || user.name}
+                        onChange={(e) => setName(e.target.value)}
+                        ></input>
                     </div>
-
                     <div className='pro-txtemail'>
                         <p className='txtemail-pro'>
                             Email:
                         </p>
-                        <input className='ipt-txtemail' placeholder='Your email...'></input>
+                        <input className='ipt-txtemail' placeholder='Your email...'
+                        value={email || user.email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        ></input>
                     </div>
-
                 </div>
-
                 <div className='list-tt'>
                     <div className='pro-txtname'>
                         <p className='txtdate-pro'>
                         Date of birthday:
                         </p>
-                        <input className='ipt-txtdate' type="date" ></input>
+                        <input className='ipt-txtdate' 
+                     
+                        placeholder='dd/mm/yyyy'
+                        value={ birthdate || user.birthdate  }
+                        onChange={(e) => setBirthdate(e.target.value)}
+                        ></input>
                     </div>
-
                     <div className='pro-txtemail'>
                         <p className='txtemail-pro'>
                         Phone:   
                         </p>
-                        <input className='ipt-txtemail' placeholder='Your address...'></input>
+                        <input className='ipt-txtemail' placeholder='Your address...'
+                        value={ phone || user.phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        ></input>
                     </div>
-                    
                 </div>
-
                 <div className='list-tt'>
                     <div className='pro-txtname'>
                         <p className='txtname-pro'>
                         Address:
                         </p>
-                        <input className='ipt-txtname' placeholder='Your address...'></input>
+                        <input className='ipt-txtname' placeholder='Your address...'
+                        value={ address || user.address}
+                        onChange={ (e) => setAddress(e.target.value)}
+                        ></input>
                     </div>
-
                     <div className='pro-txtemail'>
                         <p className='txtabout-pro'>
                         About me:
                         </p>
-                        <textarea className='ipt-textAbme' placeholder='About you...'> 
+                        <textarea className='ipt-textAbme' placeholder='About you...'
+                        value={about || user.about}
+                        onChange={ (e) => setAbout(e.target.value)}
+                        > 
                         </textarea>
                     </div>
-                    
                 </div>
-
-
+                <button className='btn-updateprofile' onClick={handleUpdate}>
+                    <p> Update</p>
+               </button>
             </div>
 
         </div>
